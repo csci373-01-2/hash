3007936ab1a1a39edb56f1328a814cb7f484a0b4
Evan Misshula <emisshula@jjay.cuny.edu>
2015-10-01 09:55:09 -0400

    deleted the print inside the hash function

---
+++ my_inc/hash.cpp
@@ -0,0 +1,76 @@
+// Copyright 2015 Evan Misshula
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include "../my_inc/hash.h"
+
+hash::hash() {
+  for ( int i=0 ; i < tableSize; i++ ) {
+    HashTable[i] = new item;
+    HashTable[i]->name = "def_name";
+    HashTable[i]->drink = "def_drink";
+    HashTable[i]->next = NULL;
+  }
+}
+
+void hash::AddItem(string name, string drink) {
+  int index = Hash(name);
+  if ( HashTable[index]->name == "def_name" ) {
+    HashTable[index]->name = name;
+    HashTable[index]->drink = drink;
+  } else {
+    item* ptr = HashTable[index];
+    item* newPtr = new item;
+    newPtr->name = name;
+    newPtr->drink = drink;
+    newPtr->next = NULL;
+    while (ptr->next != NULL) {
+      ptr = ptr-> next;
+    }
+    ptr->next = newPtr;
+  }
+}
+
+int hash::NumberOfItemsInIndex(int index) {
+  int count = 0;
+  if (HashTable[index]->name == "def_name") {
+    return count;
+  } else {
+    count++;
+    item* Ptr = HashTable[index];
+    while ( Ptr->next != NULL ) {
+      count++;
+      Ptr = Ptr->next;
+    }
+  }
+  return count;
+}
+void hash::PrintTable() {
+  using std::cout;
+  using std::endl;
+  int number = 0;
+  for (int i = 0; i < tableSize; i++) {
+    number = NumberOfItemsInIndex(i);
+    cout << "----------------------\n";
+    cout << "index = " << i << endl;
+    cout << HashTable[i]->name << endl;
+    cout << HashTable[i]->drink << endl;
+    cout << "# of items = " << number << endl;
+    cout << "----------------------\n";
+  }
+}
+int hash::Hash(string key) {
+  int hash = 0;
+  int index = 0;
+  using std::cout;
+  using std::endl;
+
+  for ( int i = 0; i < key.length(); i++ ) {
+    cout << "key[" << i << "] = " << static_cast<int> (key[i]) << endl;
+    hash += static_cast<int> (key[i]);
+  }
+  index = hash % tableSize;
+  return index;
+}
---
+++ my_inc/hash.h
@@ -0,0 +1,28 @@
+// Copyright 2015 Evan Misshula
+
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#ifndef MY_INC_HASH_H_
+using std::string;
+#define MY_INC_HASH_H_
+
+class hash {
+ private:
+  static const int tableSize = 10;
+  struct item {
+    string name;
+    string drink;
+    item* next;
+  };
+  item* HashTable[tableSize];
+ public:
+  hash();
+  int Hash(string key);
+  void AddItem(string name, string drink);
+  int NumberOfItemsInIndex(int index);
+  void PrintTable();
+};
+
+#endif  // MY_INC_HASH_H_
