0b724abf36ebc11a3fa532f86bbce8cc5b2e3599
Evan Misshula <emisshula@jjay.cuny.edu>
2015-10-01 09:44:16 -0400

    implemented chaining

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,20 @@ int main(int argc, char *argv[]) {
   using std::cout;
   using std::endl;
 
-  index = hashObj.Hash("EvanM");
-  cout << "index = " << index << endl;
+  hash myHash;
+  myHash.PrintTable();
+  myHash.AddItem("Paul", "Locha");
+  myHash.AddItem("Kim ", "Iced Mocha");
+  myHash.AddItem("Emma", "Strawberry Smoothy");
+  myHash.AddItem("Annie", "Hot Chocolate");
+  myHash.AddItem("Sarah", "Passion Tea");
+  myHash.AddItem("Pepper", "Caramel Mocha");
+  myHash.AddItem("Mike", "Chai Tea");
+  myHash.AddItem("Steve", "Apple Cider");
+  myHash.AddItem("Bill", "Root Beer");
+  myHash.AddItem("Marie", "Skinny Latte");
+  myHash.AddItem("Susan", "Water");
+  myHash.AddItem("Joe", "Green Tea");
+  myHash.PrintTable();
   return 0;
 }
